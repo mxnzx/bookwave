12c939b243b22d9fc3fb21f63fd83c52cf7766a7
junho <sseq007@naver.com>
2023-09-11 17:25:30 +0900

    rm : txt 제거
    Former-commit-id: 6825cdb7d7451a49186744c0cc121a5e79b6dad9

--- FE/src/components/Common/HeaderTemp/Temp.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8fef3f9982fd7c079afae6fc24bd9b37b86ff570b7a5ab0366dc43f36d4e8c99
-size 1223
--- FE_TEST/src/App.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:00f300c6ca18b7e06fba39474d771df36dc1fc6afb51df5715eca6346617ef32
-size 2244
