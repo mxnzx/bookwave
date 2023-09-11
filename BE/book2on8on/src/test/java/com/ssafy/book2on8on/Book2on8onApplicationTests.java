baa1f1920f741c0d47bb825b625a47b705c12dd7
junho <sseq007@naver.com>
2023-09-11 20:14:37 +0900

    feat : first back init
    Former-commit-id: b6d4ec464896e277c95d9642a7b8b39ea5386b79

---
+++ BE/book2on8on/src/test/java/com/ssafy/book2on8on/Book2on8onApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.book2on8on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Book2on8onApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
