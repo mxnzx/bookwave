43d691935590a07930f94164dd1c415304489696
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-09-12 09:57:51 +0900

    Feat: 필수 라이브러리 설치 및 불필요한 파일 삭제 및 App.tsx 수정
    Former-commit-id: fd70d2bcd3e895a1ff3ba7fd57ca8662d7d8e343

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:072a03a361d69d925e62af6786774eb55970aa5b96a105bb0592b6c9a44d1060
-size 2203
+oid sha256:f349086ac6bbd66e937561890f84ff469e4a25ab677ea01ee6c81d4ee5a247db
+size 2188
