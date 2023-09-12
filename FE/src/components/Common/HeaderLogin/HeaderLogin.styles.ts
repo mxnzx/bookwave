807f775db33310a47f3a7d000d234227d955f2de
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-12 14:05:45 +0900

    Fix: header
    Former-commit-id: 3d783d1ad996fdde3115946db3872cfcec9ae95c

--- FE/src/components/Common/HeaderLogin/HeaderLogin.styles.ts
+++ FE/src/components/Common/HeaderLogin/HeaderLogin.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43fc88e437d0a0d4f8539faa357f1f3b895512701cb1ab4b76290d61b23f8e70
-size 1213
+oid sha256:36ec411d55c278d6450ff70214f16b22bc78298be1db8c25e463f03434a2fc9a
+size 1121
