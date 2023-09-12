94b808747a2fdd88024be16fb0a43e75e71bfa16
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-12 18:16:46 +0900

    Feat: 헤더  수정
    Former-commit-id: 42a556ad1d7963192e7397f40a53b32522236d8d

--- FE/src/components/Common/HeaderLogin/HeaderLogin.styles.ts
+++ FE/src/components/Common/HeaderLogin/HeaderLogin.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8dd19406d8ae34ab039163c2c9d1361af025f4b2079a1e042c89de56744b560
-size 1162
+oid sha256:861890ff67bed744be3c5bceeda9842e742baa72c819dcc90df3ec2ff0f7dda4
+size 1142
