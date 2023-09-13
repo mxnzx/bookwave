e0c1de752540f7435d782403f3eb8ba02f7d63d5
junho <sseq007@naver.com>
2023-09-13 10:13:55 +0900

    fix : Security 기본 설정 및 Swagger member만 설정
    Former-commit-id: 6f93b380474cebe42a60c5a564648e8a5fef05ab

--- BE/bookwave/src/main/java/com/ssafy/bookwave/global/common/config/SecurityConfig.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/global/common/config/SecurityConfig.java
@@ -1,4 +1,3 @@
-package com.ssafy.bookwave.global.common.config;
-
-public class SecurityConfig {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3b485e165ab0faf1a9c8ada5dc4230aba1a602157fee5f82a97539f0ed23ac4b
+size 951
--- BE/bookwave/src/main/java/com/ssafy/bookwave/global/common/config/SwaggerConfig.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/global/common/config/SwaggerConfig.java
@@ -1,9 +1,3 @@
-package com.ssafy.bookwave.global.common.config;
-
-import org.springframework.context.annotation.Configuration;
-
-
-@Configuration
-public class SwaggerConfig {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5b42ff95a81267bf8008cab54073a71bbb8ddb6bcef44dfa587cb8a11864c52
+size 892
