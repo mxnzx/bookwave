91ffae0fa1bcb0b5a4f04d9aeee373340845cfd6
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-15 13:40:59 +0900

    Update
    Former-commit-id: c55fce292e77766d0584e378f9e1d78e866cdbb0

--- FE/src/pages/CreateRecordPage/CreateRecordPage.tsx
+++ FE/src/pages/CreateRecordPage/CreateRecordPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3c2abcceb8485439962b344b18817dbc09877b1b2cf33cfb036e65b62630b9f
-size 1137
+oid sha256:e2da2b150cde32a420ac4ff15c41f4408780a7f281b0a62b35521a9b9afd7263
+size 1135
