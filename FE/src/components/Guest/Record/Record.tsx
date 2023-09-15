e62bdbb7166f189d9e4aefe89068bcd894e1d4fd
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-09-15 17:53:05 +0900

    Design: 게스트 페이지 CSS 수정 중
    Former-commit-id: 45a11d9bd5071aa60d45157bdc71c585d437743e

--- FE/src/components/Guest/Record/Record.tsx
+++ FE/src/components/Guest/Record/Record.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6c929399c979ed6671a1925d3b4bbe5948ee8ebc993294ede55bd97283cf0e7
-size 1417
+oid sha256:fee3c7e4dbaa6ae193f8be5ca97cf0442bee37bf09e120af3e4303b649a6c6fb
+size 1520
