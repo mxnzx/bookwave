ecf5c14234ed8a4be988b1205e7d48288e60ff6f
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-17 22:57:23 +0900

    Feat: 리마인더 수정_09172257
    Former-commit-id: ffbd3d20d2e5363640556234b2742db4610e4461

--- FE/src/components/Feed/Reminder.styles.ts
+++ FE/src/components/Feed/Reminder.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ba13d3e9e3e80d024d774d2f4549b3370d4fd26e8e627e2ab109b7fc1a2b6be
-size 3057
+oid sha256:e937afdd1663f557b4c7b6ab74f7de2db0ef03a1bbe93934e92342f0d61baca2
+size 3283
--- FE/src/pages/FeedPage/FeedPage.styles.ts
+++ FE/src/pages/FeedPage/FeedPage.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bc930f51fae17b2d2aba384c1180d404ccaa830a1c4e1659166a70e7f51c635
-size 3087
+oid sha256:50f4c95c591227ad92a703c941b853160acb12182ffd7685aa972f80ca26a640
+size 3093
