da18989f358412bdb22316d13baefe97dd9be26b
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-09-17 11:32:15 +0900

    Feat: 추천 페이지 디자인 작업 - BBTI 기반 추천 디자인 완성 - recoil/atoms.ts 파일 추가 : 임시 BookList 데이터 저장 - refresh btn assets 폴더에 저장
    Former-commit-id: f91a1a25db81d3b1132166f5f15acdc877a5c952

--- FE/src/pages/RecommendPage/RecommendPage.tsx
+++ FE/src/pages/RecommendPage/RecommendPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a247e5999c3308d781a893b0872dbe173c34915ab37fb7cd850c6921f27d7c7e
-size 544
+oid sha256:c5810e4d946abbb6cb73c72e59668af957a50b7f20d4ade436e1b0722d3ea5ab
+size 4505
