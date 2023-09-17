b4a50d0e2b4c6afa0cc7544606a6dd331f69a8d5
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-09-17 12:09:14 +0900

    Style: 높이 및 색 변경
    Former-commit-id: 1a21651b527410aa908e8ed1e939762211b801ef

--- FE/src/pages/RecommendPage/RecommendPage.tsx
+++ FE/src/pages/RecommendPage/RecommendPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5810e4d946abbb6cb73c72e59668af957a50b7f20d4ade436e1b0722d3ea5ab
-size 4505
+oid sha256:cc53fca1e2f8f8abb03f1f128fcf1f347ec95bd3ef80b72eb10dd1727514216b
+size 4517
