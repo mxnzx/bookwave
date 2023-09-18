2122c037a81c4a847b73d97009f0bb5239db852d
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-19 00:41:16 +0900

    Feat: 1차 완
    Former-commit-id: ccbd3f20eda51c49bcf765d9fd562d9f2febb089

--- FE/src/pages/CreateReminderPage/CreateReminderPage.tsx
+++ FE/src/pages/CreateReminderPage/CreateReminderPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:503305414d9308a406e04c5269c434ca17518db3dacd540df2c490a2278d4d4f
-size 1587
+oid sha256:863d10975d3286ffe280735d9d4033aa6b4ed05fc31e76ef86f2ee4d6277e2dd
+size 1593
