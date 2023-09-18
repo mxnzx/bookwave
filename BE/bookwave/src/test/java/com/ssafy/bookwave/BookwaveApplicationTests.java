5f451455b3218c862d0c922ef388369f61960b8e
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-09-19 01:12:00 +0900

    Rename: Test 클래스 이름 변경
    Former-commit-id: 8b1c2791de76265a8ea846367bbf8e45751a85ce

--- BE/bookwave/src/test/java/com/ssafy/bookwave/BookwaveApplicationTests.java
+++ BE/bookwave/src/test/java/com/ssafy/bookwave/BookwaveApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Book2on8onApplicationTests {
+class BookwaveApplicationTests {
 
 	@Test
 	void contextLoads() {
