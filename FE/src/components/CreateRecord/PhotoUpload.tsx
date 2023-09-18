dddcc7f9db3fc5ec1db41ece95e2c8ffb8aac447
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-18 22:57:14 +0900

    Feat: 사진업로드 수정
    Former-commit-id: 2737b863bb565d824e57c67fd23e57d7c268e0d1

--- FE/src/components/CreateRecord/PhotoUpload.tsx
+++ FE/src/components/CreateRecord/PhotoUpload.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27ba4b19ed595d867a99b0580ed76c403b6d00eb6e821e2cf3ef10c1d6816964
-size 2167
+oid sha256:9165652127a7b1092d17dad9c5e8ec43ede6af15fc06b7dfb6264ab0e67c9975
+size 2007
