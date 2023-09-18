e349bc82b1adc8c21b70033c5430c1a3e178a16b
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-18 17:26:19 +0900

    Feat: Text 박스
    Former-commit-id: 56aa8f697cdd345a6046b2e1762540bdcead03ee

--- FE/src/pages/CreateRecordPage/CreateRecordPage.styles.ts
+++ FE/src/pages/CreateRecordPage/CreateRecordPage.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bb9bb2d96e6b5544455d0b676a6f172c63fe311a3644a39d478fb7a8f75defc
-size 2621
+oid sha256:fc435849e126e0efd4eff2daeead9c20b3100c40b6b48f8f0f370e6908df1bc5
+size 3696
