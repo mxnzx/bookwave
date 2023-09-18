09df4dbd0d42e0ae6dd73aa48f611b311acd0b88
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-19 00:15:10 +0900

    글귀 페이지
    Former-commit-id: 08d14bb6080b02015c5b52545249cc5953511266

--- FE/src/pages/CreateReminderPage/CreateReminderPage.styles.ts
+++ FE/src/pages/CreateReminderPage/CreateReminderPage.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:505c0e83b5350dfc4b2e3b6da7c79656bd5482377582bb28a44a23a0b81a32a4
-size 3357
+oid sha256:ccb32744b977f31e6818c4559d9fa8793751605b65ba7a999c3821785cf417db
+size 3772
--- FE/src/pages/CreateReminderPage/CreateReminderPage.tsx
+++ FE/src/pages/CreateReminderPage/CreateReminderPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e88b2ff730de496f3b204f019fc7a9614c41f3cac35df723fb2cb34d62fdf2d5
-size 1426
+oid sha256:503305414d9308a406e04c5269c434ca17518db3dacd540df2c490a2278d4d4f
+size 1587
