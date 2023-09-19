98447239c4d965be79bd4d577b1a25305720e522
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-19 12:58:43 +0900

    Feat: 다 날아갈뻔
    Former-commit-id: 7f6a5c386a5c95384ba6e078746a8879af14b197

---
+++ FE/src/components/modals/RecordDetailModal/StarRating.styles.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef43ec592184889accf46901d561970c8eef2c9eceb4da04e8b22104949424e4
+size 1216
---
+++ FE/src/components/modals/RecordDetailModal/StarRating.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c111d918dcc80fa5501d89e323a8e73ab9081cfc414a27c770623f70feeb5912
+size 836
