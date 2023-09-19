d8387d967316cdecdacc87e30199cbfa0aa48206
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-19 18:34:10 +0900

    Feat: 검색창 수정
    Former-commit-id: 9a80867cc923e8ab48b41508af726b53fa934314

--- FE/src/components/CreateRecord/Calendar.styles.ts
+++ FE/src/components/CreateRecord/Calendar.styles.ts
@@ -1,17 +1,19 @@
 // temp
-import DatePicker from 'react-datepicker';
-import styled from 'styled-components';
+import DatePicker from "react-datepicker";
+import styled from "styled-components";
 
 export const Container = styled.div`
-    margin-top: 5px;
+  margin-top: 5px;
 `;
 
 export const StyledDatePicker = styled(DatePicker)`
-    font-family: 'Pretendard-Regular';
-    font-size: 1vw;
-    border-radius: 10px;
-    border: 1px solid rgba(0, 0, 0, 0);
-    width: 20vw;
-    height: 3vh;
-    text-align: center;
+  font-family: "Pretendard-Regular";
+  font-size: 1vw;
+  border-radius: 10px;
+  border: 1px solid rgba(0, 0, 0, 0);
+  width: 20vw;
+  height: 3vh;
+  text-align: center;
+  outline: none;
+  cursor: pointer;
 `;
--- FE/src/pages/CreateRecordPage/CreateRecordPage.styles.ts
+++ FE/src/pages/CreateRecordPage/CreateRecordPage.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67cc014910528f364b0dc9176706eb4ab0cf55cd6e81e0df8df8289f8dd4d920
-size 4391
+oid sha256:c0ed09071b1b8b25e53414bf9818bc0a43c825ff2652153c1eda009215a4d745
+size 4392
