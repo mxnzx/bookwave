629a80c0f7f9f5ec4ef241f840241dd9f2d0b29d
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-19 13:27:48 +0900

    Feat:Body
    Former-commit-id: 52406c26ba3c223262f43e19d133178960c3d80e

--- FE/src/components/modals/RecordDetailModal/BodyComponent.styles.ts
+++ FE/src/components/modals/RecordDetailModal/BodyComponent.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ed485abec18759d967422730686297f54adbffdaf074527a1966538595735ef
-size 1094
+oid sha256:1b18688e2637bdb1d500adaf6e9b05d7612a84eee158d63a3de555adcf234a52
+size 1156
--- FE/src/components/modals/RecordDetailModal/BodyComponent.tsx
+++ FE/src/components/modals/RecordDetailModal/BodyComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20e228cc6ffe6453edc9c9f355b59c5ea9fabcafe65b0323a2249d146287aa7e
-size 1729
+oid sha256:15d134d336bbbc309cd5de1e846953ec3914717b7a702e3e0731e2b775227a7e
+size 1728
