0c268ba5a501848eb8ce390fe800d1ab98b19c80
choijehyuk <cjh970324@naver.com>
2023-09-20 01:08:47 +0900

    Feat: 리마인더 삭제하기 구현
    Former-commit-id: c94d680334f0c15942fe2e62921558ef1b5edb78

--- BE/bookwave/src/main/java/com/ssafy/bookwave/reminder/service/ReminderServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/reminder/service/ReminderServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78f53a29fd5e244257b6aa221679f18211ab604f4ee5a5f4febab4e3f2740c9a
-size 4475
+oid sha256:f3845f2296d92fd1285eed60d046946584f978a05ae32405bb7caf81a74ab0d3
+size 4702
