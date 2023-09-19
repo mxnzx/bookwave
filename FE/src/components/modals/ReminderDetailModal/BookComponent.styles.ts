21bfaeea1c7d0b983560a1922b06c958e7681362
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-19 17:39:48 +0900

    Feat: 1차 완
    Former-commit-id: 82fd75d2caeb49dc35e8457990ba1ddc5391db98

---
+++ FE/src/components/modals/ReminderDetailModal/BookComponent.styles.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d41e983a52f4845c79d5fc0113c599c1c031a9677854d2d047337f1ee2d0d752
+size 741
--- FE/src/components/modals/ReminderDetailModal/ReminderDetail.styles.ts
+++ FE/src/components/modals/ReminderDetailModal/ReminderDetail.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33c5008e7b8dbd1ba2b8646dd80dadb5c1395be3377a7abe406562c87b92cbf5
-size 526
+oid sha256:7fafbaef649e117daeb5f854cf45b3bd6e956defc7bce4bbba341535664f349d
+size 2291
