9c3dead95417c73ce8d3fdb53c00e859940c5020
choijehyuk <cjh970324@naver.com>
2023-09-19 13:31:54 +0900

    Feat: config 설정 추가
    Former-commit-id: 33bd9699fe6c77c7b40467e5dbdcb331fca29385

--- BE/bookwave/src/main/java/com/ssafy/bookwave/global/config/RedisConfig.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/global/config/RedisConfig.java
@@ -1,4 +1,3 @@
-package com.ssafy.bookwave.global.config;
-
-public class RedisConfig {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:50c3064997f33639c204cb2955ef615695ce9991fe6908cc47b43677c4ae6b24
+size 2585
--- BE/bookwave/src/main/java/com/ssafy/bookwave/global/config/WebMvcConfig.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/global/config/WebMvcConfig.java
@@ -1,4 +1,3 @@
-package com.ssafy.bookwave.global.config;
-
-public class WebMvcConfig {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff84e7c823b69e6ecbcd3bf7d62d31f464d56fc33f2908380364078d6a673be1
+size 1316
