5672b158329846aa19b3c3e0faa2bddb4cacc694
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-09-20 11:21:30 +0900

    Style: Genre Recommend Responsive font size fixed
    Former-commit-id: c3e412bf673caf24e125b87d5c11af15fe8f96d9

--- FE/src/components/Recommend/Recommend.styles.ts
+++ FE/src/components/Recommend/Recommend.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2aa5598d8d824027f07a5a984c0924c07c58d581141fac378163a9d6b857f398
-size 6291
+oid sha256:51b7d774288a4dc847bac9cd39b35803ccb9e097b3f37015bd24eea45095c92b
+size 6348
