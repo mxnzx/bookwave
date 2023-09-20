52f960b9fa57e468c5c6be78c09e794e05d85499
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-09-20 22:22:34 +0900

    Feat: Sidebar Animation 구현
    Former-commit-id: 4341c62c84d6ce860497bfbb830f710181414f3c

--- FE/src/pages/RecommendPage/RecommendPage.styles.ts
+++ FE/src/pages/RecommendPage/RecommendPage.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a40a1216d116457ebed9d1986fc2f1e5f9b5f2c2e0d6fbfeb29d5be555146634
-size 1739
+oid sha256:205cb3f525e418d26c14f53dcf1a59df217f6db6b569c70aebe6301b2f4d50d8
+size 2076
