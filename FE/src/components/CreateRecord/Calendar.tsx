c872ed047bd508cf44a01e326c078b3457b13a77
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-20 16:34:57 +0900

    Feat: 달력 컴포넌트 수정
    Former-commit-id: ccc2d80fea8f3c3c178e06c21b144cd40d54b514

--- FE/src/components/CreateRecord/Calendar.tsx
+++ FE/src/components/CreateRecord/Calendar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:514c737729dd673e14cd9166d3c1415c361c53ce3f5c14b67e2c012fb3da4201
-size 1101
+oid sha256:8ad8bc0d6d9bb982498eaa13ecb4ebb85bda47a630afddcff6fb2c9578330bf1
+size 1198
