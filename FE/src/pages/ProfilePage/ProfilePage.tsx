532032535402208ba44c8f5f9e092ae2de7af690
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-09-20 22:54:47 +0900

    Feat: ProfilePage 버튼 디자인 수정
    Former-commit-id: bea334852a8b8c230d0f55f1119d57bf0826ed17

--- FE/src/pages/ProfilePage/ProfilePage.tsx
+++ FE/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b66bfd6cd2a6529bcb861305cb349ddc2cf78b52bcffba5d05acb5661b5f0069
-size 5390
+oid sha256:2d1b3ec057d6e22972bbdc47cfaae92de9f5244f9a16d3acf0c3fced84852094
+size 5503
