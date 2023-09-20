2a62d2e2daa9168fa5e001f147f41817ed867e2d
junho <sseq007@naver.com>
2023-09-21 00:18:19 +0900

    feat : fastapi model 적용 완료
    Former-commit-id: 555f19842958d2849e0b4c4a09aed05a6656ce12

---
+++ FASTAPI/bookwave/util/bert_classifier.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd76addb9aa70758d03b8985f35783a29e76b84c3e44cf836e35cd13ab5ea687
+size 1260
---
+++ FASTAPI/bookwave/util/modelutil.py
@@ -0,0 +1,7 @@
+import logging
+import pickle
+import pandas as pd
+import re
+
+from common.config import settings
+
