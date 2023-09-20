7441533575cbe51cce1b8d5e4041ad45c2496055
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-20 11:19:48 +0900

    Feat: 1차 완
    Former-commit-id: 80c14772e20659bd83aad584114f898f13e9aaf0

--- FE/src/components/modals/RecordDetailModal/HeaderComponent.tsx
+++ FE/src/components/modals/RecordDetailModal/HeaderComponent.tsx
@@ -1,18 +1,3 @@
-import * as S from "./HeaderComponent.styles";
-import TempImage from '@/assets/Images/TempImage.jpg';
-
-function HeaderComponent() {
-    return (
-        <S.Header>
-            <S.Image src={TempImage} alt="Temp Image" />
-            <S.NickName>
-                SiverCastle
-            </S.NickName>
-            <S.TextRight>
-                팔로잉 or OO형
-            </S.TextRight>
-        </S.Header>
-    );
-}
-
-export default HeaderComponent;
+version https://git-lfs.github.com/spec/v1
+oid sha256:61fe4053fc8b0a2b46ce444c5056fb02bb4ff07d2a0195d6ea143d9f7e35b4fd
+size 825
---
+++ FE/src/pages/RecordEditPage/EditPhotoUpload.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec4f2039e8adb325007803a3300b258bfc1c3456ae025518b5c50340dc60c6e3
+size 2164
