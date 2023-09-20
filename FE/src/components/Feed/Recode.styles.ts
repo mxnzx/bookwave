35daded25d4306245114350dae49f651a69f5cd7
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-20 13:11:35 +0900

    Feat: 피드 모달 코드리펙토링
    Former-commit-id: be8675384bc68c477248aa1f5869054649025f7f

--- FE/src/components/Feed/Recode.styles.ts
+++ FE/src/components/Feed/Recode.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9899b7c298638411169aebd34a9fdae4e6a8d47b7ed03fab3c364d3e6e57a7a
-size 4627
+oid sha256:810cba4cf49b0b9ebd65239d1ffaf46cde86123b62a1760bfb26406742ba8cb7
+size 5072
--- FE/src/components/Feed/Reminder.tsx
+++ FE/src/components/Feed/Reminder.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c41f0b45958d1de331021135860873c1bb20e2e623111be13bca56501b09260
-size 2845
+oid sha256:b6e2fced3cc12ed386bcfdf6572e24c8f22584016cf79528099231c840ade0c3
+size 3491
