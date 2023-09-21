04d0f5e3263e0a26b7ccaa21d72db8d87d1ddf3b
박소현 <park.sohyun0430@gmail.com>
2023-09-21 15:57:33 +0900

    Feat: Responsive Footer 구현 - media query 적용 - width :480px 미만일 때 footer 보임 footer 보임과 동시에 헤더에 메뉴 없앰 footer에 들어가는 아이콘들 추가 searchPage 초기화
    Former-commit-id: 2fbab20933d0254cd60fbd07fd6e4c36265f20a0

---
+++ FE/src/components/Common/Footer/Footer.styles.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bb751df4cd9a1ec89b65025296573f2a188b0195e8045aec40167a7dedcf13f4
+size 806
--- FE/src/components/Common/Header/Header.styles.ts
+++ FE/src/components/Common/Header/Header.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9caeef9ff41187af49fdbec1de9411b4a65949f500ee572a06315d154594a49e
-size 1754
+oid sha256:5bf7ea56900230f44b20abf6905b8045eb22f58bde7d0f75083dbe6bd6b17414
+size 1805
