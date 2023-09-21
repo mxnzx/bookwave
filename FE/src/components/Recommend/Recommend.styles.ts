ef6dae96933d7229829a398d6d1d325b55e72e7b
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-09-22 00:52:16 +0900

    Style: animation 설정 변경
    Former-commit-id: 97f282dddf9dfd6ec371d8f47d9ebc24923b184e

--- FE/src/components/Recommend/Recommend.styles.ts
+++ FE/src/components/Recommend/Recommend.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b604d6f88df944e85586187b9eafbdd3d3a9b3a813ed59fc100f58ab90ddf1d9
-size 6348
+oid sha256:8841afe34413bf1a53102a98bca9a563ea343799b162c28fddde76e4091c7df1
+size 6447
