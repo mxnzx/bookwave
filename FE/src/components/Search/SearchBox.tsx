65cc395c14d0c3e6c55777a11a93a97f0a06b2ab
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-21 17:11:27 +0900

    Feat: 3차 완
    Former-commit-id: d68bf250e0a36351ac8f3c0be7dbb39c2aec57c0

--- FE/src/components/Search/SearchBox.tsx
+++ FE/src/components/Search/SearchBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d80c3875fcf503218f8ba64cdfc392d59dd16903dd02d469e4f83ed93cd35888
-size 2620
+oid sha256:d010eeb8822855bf63ba9e4db2b309943e9bc29774884f221d3129a776c7e310
+size 2616
