1dc8cf9c4410a39c867d2443a961599b7bff46cd
choijehyuk <cjh970324@naver.com>
2023-09-21 16:20:17 +0900

    Feat: follow 기능 구현완료
    Former-commit-id: 0910ef880a831e9956ba0c65af8434288e9cffad

--- BE/bookwave/src/main/java/com/ssafy/bookwave/member/controller/FollowController.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/member/controller/FollowController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07977b05328ffd1e4ac0179e0e343d1e9578acd20952b69da8313e281d6b1f46
-size 3888
+oid sha256:4c7ab325a175449aebabc062d853964e1306fefe76a02db13be347c43ddc435f
+size 3864
--- BE/bookwave/src/main/java/com/ssafy/bookwave/member/repository/FollowRepository.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/member/repository/FollowRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff4b04934f1b09622fd1154597c483bc30218e3587778d6b158e208f3cc3e7e8
-size 740
+oid sha256:bfdad6ac891ba7ee20bce1e5102563b7f91ff99a461e07145dd70e84b13ce60d
+size 746
