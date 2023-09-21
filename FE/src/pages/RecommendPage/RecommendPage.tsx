8d45fe90a5185acdb7c8f81230d3e2456827f162
박소현 <park.sohyun0430@gmail.com>
2023-09-21 13:32:43 +0900

    Style: 주석 제거
    Former-commit-id: 88a259b676da5287ba86ece20fb143c7bca4a630

--- FE/src/pages/RecommendPage/RecommendPage.tsx
+++ FE/src/pages/RecommendPage/RecommendPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc40fd371e22dba16899a7cdee1c58268af1cf218146340c052ba64eb9cd19a2
-size 3517
+oid sha256:6c1c99f10165e815c1647582a947078db21d43174147bc6d665dffacf997aef3
+size 2917
