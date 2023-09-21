f4f9b4ca85ea9bba23e3e41c5fe4421ef0998dd8
박소현 <park.sohyun0430@gmail.com>
2023-09-21 16:10:02 +0900

    Style: 추천 Page margin-bottom 추가 - 스크롤 시 내용 잘리는 오류 해결
    Former-commit-id: 031acbbeaf3231694ba4f0c788eb7b501dace6b3

--- FE/src/pages/RecommendPage/RecommendPage.styles.ts
+++ FE/src/pages/RecommendPage/RecommendPage.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93f2853e4f9aedd7ed01c79cc1ed599f0d4b9b1ae7f4e6378191af22b8d1d630
-size 2215
+oid sha256:111f90783557e20bf0602216495bcef9ef4754d431c3022573ca48f3b96dcbc2
+size 2240
