11dee5395c0629e3bdef652ca83a15e673637e7c
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-22 16:41:45 +0900

    Feat: 피드페이지 구성1차완
    Former-commit-id: 7bd51886d74cecc18a4af5be53311634fb15579c

--- FE/src/components/Feed/Recode.tsx
+++ FE/src/components/Feed/Recode.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b66aa4ae7d718311ee7bd5470ef5437d990cf1953a5836fd35281acf4b8b990c
-size 4357
+oid sha256:df11684418797df269dd612a56c3c4420b5d05ae5bc18595d90492fb9a0d8333
+size 4364
