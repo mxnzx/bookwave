096dcbbd0296a70479120e4af6e41cc3c7ecd847
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-22 22:01:10 +0900

    Feat: 선택모달 2차완
    Former-commit-id: f2e7016fa07504dee6a729732f7e7a39bf7ad76a

--- FE/src/components/modals/FeedBookSelectModal/FeedBookSelect.styles.ts
+++ FE/src/components/modals/FeedBookSelectModal/FeedBookSelect.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9a0c45a2d27256a257ee0a0958f6e445e8a44db12b0c88536560fa91db62b3b
-size 3597
+oid sha256:e7080aa4d8c932473fce2c64f98318c5bd2ff018a1175cf69ca591c10589dcfd
+size 3709
--- FE/src/components/modals/FeedBookSelectModal/FeedBookSelect.tsx
+++ FE/src/components/modals/FeedBookSelectModal/FeedBookSelect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab55579ecab3ade79488d83b384016abce3b68a18364128784b73eb559cf22b1
-size 6278
+oid sha256:fdc081086f40e26f748e78135a4fc0866154896a9d90675ef41102dda1ad4411
+size 7197
