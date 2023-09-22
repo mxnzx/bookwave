f1ddc412e50bb6c40111b20f560c7f92504037e6
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-23 00:49:29 +0900

    Feat: 독후감 2차완
    Former-commit-id: 7514118f3b7f08ef667bd7e446c391776f309256

--- FE/src/pages/CreateRecordPage/CreateRecordPage.styles.ts
+++ FE/src/pages/CreateRecordPage/CreateRecordPage.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4df5baf55969164155968d8d3b774f64d19843134f1f5e0ae40c57f7e0e00905
+oid sha256:18d742db4f081c4f8d820864b7217a92d50e28a5a7b5ec1e79f64df42c6b6bc1
 size 3857
