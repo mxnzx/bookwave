4c09947a6158f8a9e4d842117aaf4f443d824ea2
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-23 20:35:19 +0900

    Feat: 도서 디테일 바디
    Former-commit-id: 01a9d73a118d4a1c07f3e210eee5f9a4b9fe86ea

--- FE/src/components/modals/RecordDetailModal/BookComponent.styles.ts
+++ FE/src/components/modals/RecordDetailModal/BookComponent.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4c8bf9782c04c3ed6c6e8885b7ad2bd3bfb5764a75d74f4c5012290ac6e6696
-size 924
+oid sha256:138bf0a371f690b8d7bb839c894dff992ddb5b5b7002c3b475a153b09a3b4867
+size 938
--- FE/src/components/modals/RecordDetailModal/RecordDetail.tsx
+++ FE/src/components/modals/RecordDetailModal/RecordDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d75fd86fe62b54b376d125863d39bf5881e7fdc4ba0387f40e0d1569c6e9378c
-size 1179
+oid sha256:1265858bf737069f405ef3384de0cd2aa45389d93ce47d2baf22f2800666b5f7
+size 1155
