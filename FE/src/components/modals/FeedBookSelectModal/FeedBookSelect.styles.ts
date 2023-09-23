9a87221d564780c20cacf0e9c8ac9d95b4801614
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-23 16:27:53 +0900

    Fix: 선택 모달
    Former-commit-id: a6b8fe21fa143dec2c879d46b43585306d2c4d11

--- FE/src/components/modals/FeedBookSelectModal/FeedBookSelect.styles.ts
+++ FE/src/components/modals/FeedBookSelectModal/FeedBookSelect.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:474191f2b4a0cb82d17b5f9823c77a59ad42089dd5aedda1d42bb22c90932509
-size 3767
+oid sha256:62ad83038be33641586cc775a39dc35503e029a6a8f2af8af91feab1cd22845b
+size 3894
