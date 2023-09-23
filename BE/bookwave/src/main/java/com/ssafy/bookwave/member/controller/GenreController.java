11eea547490f2c45ab2f4c452e96f9b05225894f
choijehyuk <cjh970324@naver.com>
2023-09-23 14:56:45 +0900

    Refactor: response 변경
    Former-commit-id: 1ebdea234c6e84e28f5cc383760325fb48139d5e

--- BE/bookwave/src/main/java/com/ssafy/bookwave/member/controller/GenreController.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/member/controller/GenreController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da59ee67dab207e500acfecf4303288b02ade49eea157bf36bc7a5ccfbf47b8b
-size 2288
+oid sha256:72814a3ab8e70d40cb1ff45791b367bc278f5841b23d84421b6ab1f44f2d3299
+size 2306
