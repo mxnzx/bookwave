ca45d58b8bbc702710aad96b0a945ba91881066b
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-24 18:18:13 +0900

    Feat: 로딩스피너
    Former-commit-id: 75411faa467853a7d40e31526a29d4cc58818f0b

--- FE/src/pages/CreateRecordPage/CreateRecordPage.tsx
+++ FE/src/pages/CreateRecordPage/CreateRecordPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a30634cb498b39e4472701c24fc4cb513246a27d3a6c434fb762b2d0e619ede5
-size 2560
+oid sha256:d26d4c14bad6bf13ff4bf88a84735e2a631fec40dd895839f9666d3d74243eba
+size 3044
--- FE/src/pages/CreateReminderPage/CreateReminderPage.tsx
+++ FE/src/pages/CreateReminderPage/CreateReminderPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab0d3e607dd4aa212a92bbfe2368096e21bb4f44cb0e40a7fda82691c2547d41
-size 2287
+oid sha256:2329a00700b75ed81aebe12373f28af049469497d8245bad71c5b18c7c15b025
+size 2669
