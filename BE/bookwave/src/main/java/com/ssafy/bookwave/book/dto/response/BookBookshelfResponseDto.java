576ea6a205c0af5dd83bc4a5373c81eb4374e3fb
choijehyuk <cjh970324@naver.com>
2023-09-24 16:00:34 +0900

    Feat: 독후감 등록 시 책장에서 책 목록 불러오기  bookId 추가
    Former-commit-id: 4b90a48a6588a4d109b3034182b34e41ed5d9494

--- BE/bookwave/src/main/java/com/ssafy/bookwave/book/dto/response/BookBookshelfResponseDto.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/book/dto/response/BookBookshelfResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d18988347bafba964e0cd7b588dc3b720303029cc7d0b48b28fc0a81f1ed78df
-size 565
+oid sha256:8dd2727e9091e49aef38cf97bf329f579d759d6d375414524b6abaa8307e80aa
+size 630
--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:557c8e3cb93b56f486392f992e1f8da16716e56cfa54faf2e4178f8624d1e40c
-size 3555
+oid sha256:ba77b06f92621a738c6e1b2f7faf8f6f98de84fb54904176a7385cb18c8280be
+size 3667
