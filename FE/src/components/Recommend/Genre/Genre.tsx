b454e829de3efdce8b721b8d1697ddf75862577b
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-09-25 22:59:31 +0900

    Style: Recommend Style Change
    Former-commit-id: 3c204a813c7c0a76aae21bd07c2b208549cd0cc5

--- FE/src/components/Recommend/Genre/Genre.tsx
+++ FE/src/components/Recommend/Genre/Genre.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62446c6fa55c4e67264e6d019c253f7189f87dadd3cc1f0aaa029afd687ee523
-size 3274
+oid sha256:53ff0bd2159c800e39993fc90d659436eb2bb5493008efe45f0f406378172245
+size 3260
--- FE/src/recoil/atoms.ts
+++ FE/src/recoil/atoms.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:394f960e3a5baee1085d00d93f28aadd869cd0a9dd82d7402d30f8c8023c21e6
-size 28723
+oid sha256:f003a6741d032b008415c58cd72c1f6b030ed5bf5e575a4ab8e1c14396c33743
+size 28713
