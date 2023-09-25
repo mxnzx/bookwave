15aa8d3e6c1b50223e1a9e566eb6e1c94c4ca97e
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-26 00:10:57 +0900

    Feat: 피드 BBTI 유형 설정
    Former-commit-id: 89baacbf2e247c30744e87b0c1357d5bce7d8abf

--- FE/src/recoil/atoms.ts
+++ FE/src/recoil/atoms.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:394f960e3a5baee1085d00d93f28aadd869cd0a9dd82d7402d30f8c8023c21e6
-size 28723
+oid sha256:fea0bacd18440022f96829e2e650465d4fc2d10208335bc9bdc4d7f85d580819
+size 34582
