2fb3c14b942dc3b4745f69171f331731bf8dbe39
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-25 15:20:20 +0900

    Feat: Axios 1차연결
    Former-commit-id: 0b8ac8453b48369a20588a1108347225dfa0b339

--- FE/src/components/Join/GenreSurvey/GenreSurvey.tsx
+++ FE/src/components/Join/GenreSurvey/GenreSurvey.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:830f5cfb5ee91c99f020631084560f10a7c28903cbdaadc0a3aca5cdb70b78b0
-size 2941
+oid sha256:adeef76f6a7081010d2c53dc4db999a4e6351f0cbe186e4bd930bac518029e16
+size 2924
