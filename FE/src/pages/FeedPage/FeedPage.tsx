ebe30e3940ff942e38433701b533ad0c9c551dde
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-25 22:19:45 +0900

    FEAT: 선택모달 반응형
    Former-commit-id: 52de30c5be233562ce389175d4bf5be274e62735

--- FE/src/pages/FeedPage/FeedPage.tsx
+++ FE/src/pages/FeedPage/FeedPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd387168cba7bfc9dafd78f3c794a5a12a793b91b4d8b0ccb78fc5f215c8c01e
-size 3460
+oid sha256:3a0a6a2679e5541929e40547a4ea6250029f05fe32ff3c983c7fbd8b592ab1a4
+size 3466
