dc8cadfe1e514334132839b76598801770bb8fee
choijehyuk <cjh970324@naver.com>
2023-09-27 01:12:05 +0900

    Feat: 멤버 파일 경로 수정
    Former-commit-id: 218c2ece7edb011ac9c3eb496c56905154b7c985

--- BE/bookwave/src/main/java/com/ssafy/bookwave/global/config/WebMvcConfig.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/global/config/WebMvcConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b193a1cbf3b541409f31332aa59f9768831bdc1256f0fff7ad912d6c49cdc31
-size 1347
+oid sha256:74cf74406a6ddf93655c46f11c4303ca7df589c15a756f1b8b7c47eb6a0e5374
+size 1354
