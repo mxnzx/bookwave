e6c813c06e6d3fd1819e1d94dd0f2116ef2f2c6e
choijehyuk <cjh970324@naver.com>
2023-09-26 17:51:15 +0900

    Feat: 이미지 경로 지정
    Former-commit-id: a9104f89174fa0821ec1e58b09cb36444b190769

--- BE/bookwave/src/main/java/com/ssafy/bookwave/global/config/SecurityConfig.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff496f1fad4636607bdcbc947ef428ce82d77e41246e2768f57c731ffe554b29
-size 2847
+oid sha256:fed282f13d53a452ac176aded7c3397fb3ee626690fe85673f29836a2c677895
+size 2899
