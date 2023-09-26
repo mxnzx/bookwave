bcc53a66892a9e5903eb89ac9e6d3d8004470abb
choijehyuk <cjh970324@naver.com>
2023-09-26 22:50:26 +0900

    Feat: 이미지 경로 지정
    Former-commit-id: a9bbf24595157e71b23249d91b56fec60d20a49d

--- BE/bookwave/src/main/java/com/ssafy/bookwave/global/util/property/ApplicationProperties.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/global/util/property/ApplicationProperties.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86f6765fc411ae32d1def2d0ddd3a5e038ef84377999af0f94aa3f36d97176a5
-size 577
+oid sha256:e38149f173abcb878011dcbff18482083e69255499552e56e97ad307d13374a6
+size 579
