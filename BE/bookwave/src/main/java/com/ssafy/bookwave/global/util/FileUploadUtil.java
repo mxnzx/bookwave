836fec59ff4bb59c40160dad2cd3c8fe2187baea
choijehyuk <cjh970324@naver.com>
2023-09-26 22:23:51 +0900

    Feat: 이미지 경로 지정
    Former-commit-id: afa6495f9bb5eb2e91453c9a825f9e7ff8551022

--- BE/bookwave/src/main/java/com/ssafy/bookwave/global/util/FileUploadUtil.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/global/util/FileUploadUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1773846ab642891416ff725ad33a6b7ff51d9f2278a7c56fb9d0cfa52ad26049
-size 5311
+oid sha256:f1cf8b5331c2c0d89bc1f0d2d5f457c52595a4c44dfa0ce437c484e051c67807
+size 5309
