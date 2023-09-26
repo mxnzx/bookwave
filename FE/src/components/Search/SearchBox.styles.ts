dc4645adde66298206cc04d7e5da1718c663a1bc
EUNSEONG LEE <pro30343069@gmail.com>
2023-09-26 10:52:25 +0900

    Feat: 검색 페이지 반응형(검색창)
    Former-commit-id: b84335fd9b7cbdfd6fd8fb57b10c67c0783f76dc

--- FE/src/components/Search/SearchBox.styles.ts
+++ FE/src/components/Search/SearchBox.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c93d14d18bacf4396e75a7742c79c72a287455127e27e257b2b86a8cae01d8d9
-size 3641
+oid sha256:27e8a2b4e7aa69d75500751e90bdf604b13444088421c0976f9e526195c19dcb
+size 3881
