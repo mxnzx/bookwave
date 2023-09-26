f881815b077747f356b0cd284ab27e4ea07b3a9d
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-09-27 02:45:53 +0900

    Feat: 도서 상세 불러오기 성공 (도서 평점)
    Former-commit-id: 6fda18d242151d63ae63d533825d3feeb6e97585

--- BE/bookwave/src/main/java/com/ssafy/bookwave/book/service/BookServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/book/service/BookServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25b9d01b2d0729918c25d6568810f661a7ae0a3c3ad6c42235dc340930686ffb
-size 2280
+oid sha256:909d8979ad52f8ad6b00b81ead8c5f2dcd6783b2807156f81a81eb0062018689
+size 3077
