1f3d341309c392cfc3b1f99a3794f5d99c99c800
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-09-27 00:22:53 +0900

    Feat: 장르 선택 기능 수정
    Former-commit-id: c2fd9455662e6d47dad345e858e51f2a618c8660

--- FE/src/components/Common/Header/Header.tsx
+++ FE/src/components/Common/Header/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2235b415e25092f1d8f23846f41a5c841321eb86f494c1ea193bfb69cc178504
-size 4330
+oid sha256:5b4e687b992f614d592ad3bb04b366135afc6140d6f7b04669fe3738c0ea0da6
+size 4307
