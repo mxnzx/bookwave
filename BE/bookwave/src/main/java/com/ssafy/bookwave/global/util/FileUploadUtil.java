c69f2ca5e34c295732c81395c1b02442e872ba88
choijehyuk <cjh970324@naver.com>
2023-09-27 01:27:40 +0900

    Feat: 멤버 파일 경로 수정
    Former-commit-id: 1c4c4f563687275b62c50137b49b87c9105a4d8e

--- BE/bookwave/src/main/java/com/ssafy/bookwave/global/util/FileUploadUtil.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/global/util/FileUploadUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9603358aa7007d4908aad4b2b41381a6427ab6a5910831fd9c02bfe798a5c6f9
-size 3693
+oid sha256:15e8f82920bbbbd45a800bd2838a6509bd195f8bec3f95118ff23cf194d560e2
+size 3719
