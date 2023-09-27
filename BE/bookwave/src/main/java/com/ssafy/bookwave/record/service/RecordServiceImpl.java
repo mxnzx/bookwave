ddda9e5f5f259e152e31459e4b2b883ec058cb66
choijehyuk <cjh970324@naver.com>
2023-09-27 17:40:45 +0900

    Feat: 레코드 책 상태
    Former-commit-id: db4f7b43f8fd6b79fbbfd3196ecce66fd27d6743

--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:664b6b62178b9a4ed62dfe50e78ff45d4c63f3f07753d5d9e8fde170f6788be5
-size 15709
+oid sha256:35450140e2e6b869ef5da7550ad830944244ff8b1c8ee32b20199d1068af8071
+size 15999
