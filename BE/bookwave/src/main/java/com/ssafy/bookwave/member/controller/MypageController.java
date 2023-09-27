fe5402c391e5b555b6c67c52d9f22239402b9d0d
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-09-27 19:04:58 +0900

    Feat: 기록 페이지 회원 정보 조회 API 구현
    Former-commit-id: b3b2c0685de3f74f79a7f81614022b4998ea5a0c

--- BE/bookwave/src/main/java/com/ssafy/bookwave/member/controller/MypageController.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/member/controller/MypageController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28709823e15d1c0acf30410781df6d9c3f09e99a35330023aa5d77e82e8dac42
-size 1590
+oid sha256:49384b3086a559b0eed561e33ed7a91ee54094d1f4fe37738b3a1544d1db5f93
+size 1591
--- BE/bookwave/src/main/java/com/ssafy/bookwave/member/enums/MemberResponseMessage.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/member/enums/MemberResponseMessage.java
@@ -2,7 +2,8 @@ package com.ssafy.bookwave.member.enums;
 
 public enum MemberResponseMessage {
 
-    MEMBER_GETALLMEMBER_SUCCESS("회원 모든 정보 조회 성공");
+    MEMBER_GETALLMEMBER_SUCCESS("회원 모든 정보 조회 성공"),
+    MEMBER_MYPAGE_INFO_SUCCESS("기록 페이지 회원 정보 조회 성공");
     private final String message;
 
 
