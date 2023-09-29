68b7e3875ea23b00e8b5ada5fe9a9126057368ad
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-09-29 10:08:09 +0900

    Feat: 도서 상세 불러오기 성공 (도서 감정 리스트)
    Former-commit-id: 4e0b86da684dfb32242b0a7083d402225e75a43d

--- BE/bookwave/src/main/java/com/ssafy/bookwave/member/repository/MemberBookScoreRepository.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/member/repository/MemberBookScoreRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65d2fcf5274a01ec51f62bf0cc44e4094dfda34075daebe6fe397cd0cd5dc13f
-size 625
+oid sha256:590dac48dc04ac11548ae5f4689e1726913aa96176543f80e7eafaf852b8b546
+size 592
