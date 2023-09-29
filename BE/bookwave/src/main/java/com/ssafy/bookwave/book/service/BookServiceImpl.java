1b07378cb0baa12274f3a561657c93a44137da21
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-09-29 21:34:44 +0900

    Merge to feature/BE-bookDetail
    Former-commit-id: 8d093ae392114f122c585d8a00c0e8fd7bbda7cb

--- BE/bookwave/src/main/java/com/ssafy/bookwave/book/service/BookServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/book/service/BookServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc6a7c2f29059057eeedd38ecda51afe1e4a25d2781d5dcf328db7fd3af0a0f0
-size 8181
+oid sha256:1313664515451d7dbb2df6c5fc5161e92504f013f20c09c3455d2ce952085c75
+size 8185
