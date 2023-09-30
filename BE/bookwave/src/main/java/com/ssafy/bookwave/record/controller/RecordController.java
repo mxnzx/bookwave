58c204852054109afcbfc4c45d5ec92ae4a22302
junho <sseq007@naver.com>
2023-10-01 01:21:09 +0900

    Fix : 피드 컨트롤러 피라미터 수정
    Former-commit-id: 4d4ed660d4d8b4d401b3cf65ceb71bf0640ce5da

--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/controller/RecordController.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/controller/RecordController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e05b6fb04cd929751891a6f0f407882307a1ed960ccede2dee845d9c63d4e10
-size 12366
+oid sha256:f6935903a235e98267127f150a706bde2151eaba4fcbfd195a10beacd12ed450
+size 12356
