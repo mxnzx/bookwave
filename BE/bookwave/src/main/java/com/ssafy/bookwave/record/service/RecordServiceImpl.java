464cdb17c8ac3c50dd6fcc2bbfa481a57765a439
choijehyuk <cjh970324@naver.com>
2023-10-01 00:24:50 +0900

    Feat: 레코드 수정 사진
    Former-commit-id: 9a98446d01bcbeb958be969919969e88e546ba63

--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bfdae192e099f09ebc7c5c2ae3bbea10e0874bb75934af9c2f0bc0d58ca2935
-size 15736
+oid sha256:e8bd537e10f4ad222801543b71af7c729a5381dd2d87a6f199d3d605b13d79b4
+size 15738
