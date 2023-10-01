1523970a2aad6565c08fc61fc045f9a795a48f72
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-10-01 20:10:00 +0900

    Style: 주석 삭제
    Former-commit-id: 27b31c8fa1146ed345a54f496454a1ea5309e0a5

--- BE/bookwave/src/main/java/com/ssafy/bookwave/book/service/BookServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/book/service/BookServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:354d366d6ab2b8ad4071da1b6233868e8c0d4f8259f2051762a0063a6731b47c
-size 10623
+oid sha256:ca3585b40f65c7b9c1676dd4f28e37d5460679f644b2e4de35bb27077f3d787c
+size 9892
