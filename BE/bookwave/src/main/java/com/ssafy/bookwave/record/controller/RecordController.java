843b20538d302b5b2766cb304e30cf873242827c
choijehyuk <cjh970324@naver.com>
2023-10-01 21:05:21 +0900

    Feat: record fastapi 통신
    Former-commit-id: c7f535f7dacad95c8e6887d4e7c2fdb60ba907fa

--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/controller/RecordController.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/controller/RecordController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:179a698419e3af147048c1d1b33275e753be65343fbffe42b8e94a1e9b5179c9
-size 13285
+oid sha256:945c6f777007c5bdd2b75268b496f1a5b2c68d890f608c103b72501fa307dac7
+size 13282
