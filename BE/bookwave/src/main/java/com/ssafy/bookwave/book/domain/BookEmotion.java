8d6c7fff85b4c00c8b618e992b68aae105bbc313
류민지 <i0692631@naver.com>
2023-10-01 18:43:39 +0900

    Hotfix: bookemotion id 데이터 타입 변경 Former-commit-id: 368a0111421d950a990fd8df10833982dd439b41

--- BE/bookwave/src/main/java/com/ssafy/bookwave/book/domain/BookEmotion.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/book/domain/BookEmotion.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f076720add0cf89501a7107c5a1675eacedbd31929fa7a79a12f9dbaf0d91ab
-size 563
+oid sha256:0bfad00abea7f6c9c573d0d4f93b420c2f12f6d7e2144c3a51031e1fb2954267
+size 566
