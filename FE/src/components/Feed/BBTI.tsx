9a4c06ee92e25c97a6455ed808121cb9a8f8d4c2
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-01 19:50:38 +0900

    Feat: 피드 BBTI 자신 제외 검색
    Former-commit-id: 56c0e089b451704f1cdcd35f1f1e03837208faef

--- FE/src/components/Feed/BBTI.tsx
+++ FE/src/components/Feed/BBTI.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bafb85990a8b33cc86137b8275224122ec87493bd6191f875070653ece6218d2
-size 3744
+oid sha256:aadd84fb97c2cdceadd929c6e2a2a1d3680b6c0b2ddd05e0586447a9f0b26322
+size 4018
