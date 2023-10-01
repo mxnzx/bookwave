f960b8dba0dc4a653dab7f954b54937a0f438073
choijehyuk <cjh970324@naver.com>
2023-10-01 17:37:14 +0900

    Feat: 내 레코드도 보기
    Former-commit-id: 0edaa74127677ed85809e4a9f4336b16e9891ca5

--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:019a2812ca91ab2a690457b12607c766e7ecebcfe0c4579e680bdeed6bd8dc89
-size 18050
+oid sha256:d47e9cb257b057847a2163ef661e13726f59916e5923d24ec4e101b8b36809c0
+size 18204
