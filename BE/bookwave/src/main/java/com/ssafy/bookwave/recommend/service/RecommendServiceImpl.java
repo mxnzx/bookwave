06c8f522f919025b3cf5b07ee7a4e0a23667b08f
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-02 18:25:19 +0900

    Fix: Recent Recommend 로직 수정 - 각 genre detail id 별로 limit 5개로 걸었음
    Former-commit-id: d56625b3aeaa9866f189c448211c51653cdf23d9

--- BE/bookwave/src/main/java/com/ssafy/bookwave/recommend/service/RecommendServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/recommend/service/RecommendServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:280d2a5a256e1f094937656729cb6ffc4499c444c7c6c8cefca0b69b078b638c
-size 12518
+oid sha256:20cd47a45dfef1b6569d99619ec04db668cf5d3f38038ad396b2dd700d16c3e6
+size 12521
