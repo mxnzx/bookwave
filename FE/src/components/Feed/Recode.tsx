e27b8217af22c4f43e20d97f157b440041afc373
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-02 14:37:10 +0900

    Feat: 기본이미지 처리
    Former-commit-id: 0a3c66d49f09b5bcd0c5a76dd1d20c56db35d428

--- FE/src/components/Feed/Recode.tsx
+++ FE/src/components/Feed/Recode.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df9d86ed8643c5fd5635e1e35cf560d417b8b76d1e1a1703ce3f9a9cfbb8ae26
-size 6606
+oid sha256:e6ec335c7da47c7cf42fb569454baf389883c6be495abb61448f5779bd603c78
+size 6671
--- FE/src/components/modals/RecordDetailModal/RecordDetail.tsx
+++ FE/src/components/modals/RecordDetailModal/RecordDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6716909d9b75d5b0c8b9506f65d2ae3790be6e38407817f3b6ce95d8ca81e82
-size 5230
+oid sha256:fce74dbd8fb4c18e47afcddc8388b0febdd9d1fd15b87d6adda8e989bd1e4bfd
+size 5298
