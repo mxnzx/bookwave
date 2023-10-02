789f7ef7ebc1412ed0a221c8e022bdc4cdb18193
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-02 21:00:28 +0900

    fix:recent 수정
    Former-commit-id: ce164e9b8e22c8ae93bc7e09f10666851b71f7f4

--- FE/src/components/Recommend/Recent/Recent.tsx
+++ FE/src/components/Recommend/Recent/Recent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1d9510d19cb9f8b225b481551be18edd90a6631fcc894f4d2f0caa6897dbce5
-size 3433
+oid sha256:defc4cc296dc85d9ed53dfe69e427f879f17dd8fabc60bdcd1a310b920187b48
+size 3708
