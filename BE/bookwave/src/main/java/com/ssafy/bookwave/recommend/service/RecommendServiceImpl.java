ae74fa644426347b618d4e273599c5fb781f6dbc
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-02 15:42:33 +0900

    Fix: Recent Recommend 로직 수적 - 최근 읽은 책, 읽는 중인 책이 없는 경우 null처리
    Former-commit-id: 9dbe4a462474896fab73d07122fb67ed1c5251e7

--- BE/bookwave/src/main/java/com/ssafy/bookwave/recommend/service/RecommendServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/recommend/service/RecommendServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6be8f11dec7054de8346c37139f7a70689a352ecddf72775361617db3e76fee
-size 12329
+oid sha256:280d2a5a256e1f094937656729cb6ffc4499c444c7c6c8cefca0b69b078b638c
+size 12518
