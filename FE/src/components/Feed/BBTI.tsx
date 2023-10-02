54140309018dad4d9b7bc01b89a6186bb4895399
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-02 14:44:45 +0900

    Feat: BBTI 기본이미지 처리
    Former-commit-id: 14e012cb364d21de5961cdd87568a168348efad7

--- FE/src/components/Feed/BBTI.tsx
+++ FE/src/components/Feed/BBTI.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aadd84fb97c2cdceadd929c6e2a2a1d3680b6c0b2ddd05e0586447a9f0b26322
-size 4018
+oid sha256:10b381f5e1113de17fcef51b1b73f64ca17927513c754c84361ee4d95b4e7c29
+size 4069
