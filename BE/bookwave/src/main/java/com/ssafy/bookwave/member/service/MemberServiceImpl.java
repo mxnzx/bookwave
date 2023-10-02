6f64363872d91dc94eee6ceb8dfc631c83641ced
choijehyuk <cjh970324@naver.com>
2023-10-02 23:30:46 +0900

    Feat: 닉네임 미검사 시 빈 string으로 반환
    Former-commit-id: 860e36498f09ce1ab367bb9c1f97873dcbeb75af

--- BE/bookwave/src/main/java/com/ssafy/bookwave/member/service/MemberServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c0e0a10d47516d6b6043a848337b68d0e71e1c556fd41e06f6ef22e20e177a9
-size 18980
+oid sha256:ac8b94acba84aac4c9c51407ddf13caa87bb209c10603d1d41671bacb6be9bc9
+size 18924
