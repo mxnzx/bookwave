6db5d8b53df5ac430f8a41ea5c68418ff27f960b
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-03 01:21:06 +0900

    Fix: 장르별 검색 통신
    Former-commit-id: 86e0c14935c5445794d0989cb4dc0ae52979410b

--- FE/src/components/Search/SearchBox.tsx
+++ FE/src/components/Search/SearchBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd9d55f60b4faf0a85e371c194a818d46968926691ec176eaf76d404c05d4eac
-size 3153
+oid sha256:7a73afb5f4644602c4e8d88a2273762286f84e5818adfdcade3f4d4ec8e1b6e7
+size 3192
