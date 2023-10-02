2d8d1b9111300c73502f3ab87e0484a76a814ba4
choijehyuk <cjh970324@naver.com>
2023-10-03 00:38:41 +0900

    Feat: 선호장르 불러오기
    Former-commit-id: 02247ab561cc9af5f90410b0beed2ba6117d2a7e

---
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/member/dto/response/GenreGetResponseDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.bookwave.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreGetResponseDto {
+
+
+
+}
