4d45ccb237e9c01beb9b01eeae5565d961805497
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-02 20:55:04 +0900

    Feat: 밥묵자..
    Former-commit-id: 8b114a3a6e6883d50223435851cc48efb3f0aba8

--- FE/src/pages/CreateRecordPage/CreateRecordPage.tsx
+++ FE/src/pages/CreateRecordPage/CreateRecordPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a54edce8784361cbedd888aba78884bdbf12917ae670345ff6ea8daf2da4b81
-size 5768
+oid sha256:f2515cfa364744100f57daf1d4ca953cea5f521186e5d6d4099a296773b3a935
+size 5602
