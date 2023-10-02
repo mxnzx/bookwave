3e86a47ae25d857ae571d6ebd2c4dd2eee29cdd7
junho <sseq007@naver.com>
2023-10-02 15:59:08 +0900

    Feat : 글 fast api service 수정
    Former-commit-id: 573bdcbdbf7730f89a164660314e006d506e96e7

--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/controller/RecordController.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/controller/RecordController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dc101ed47d9eb3d6b78f83f12f5de60bb22ec5f9de17cda4d0a6a60fad2b33d
-size 13765
+oid sha256:e0ba2c3d1d2b82952d0509904af61f1eeb203fa303a767b448a427eda3657cd7
+size 13317
--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f14fbd8dbe731ff2c1fd697ad9a87837aa68526d8162941ef5cac3aed7378ad
-size 18744
+oid sha256:d8b9582b11e23510f50513e0ad9d51e7a2acc212f4c728148d53f987ef84de29
+size 19296
