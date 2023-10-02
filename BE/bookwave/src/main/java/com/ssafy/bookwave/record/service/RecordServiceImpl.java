986316928cbb2ade2a6911568e88c8c65f2db470
junho <sseq007@naver.com>
2023-10-03 01:21:10 +0900

    Fix : fast api 로직 수정
    Former-commit-id: 0dc7c0ea510fad94b91ac26612e3aa55d93d701e

--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:baf93928183ca35e62590ef0fa9f5e9ecfded1d03d3fc1baab1342f3074d535b
-size 19407
+oid sha256:c3ef32873ab8fbc791efe2fad0496b2f8b851326dae8041c4ac5dea21c1a2d1e
+size 19479
