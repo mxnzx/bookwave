b97f8d085260e08faafbeb78051d335d08797378
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-10-03 03:48:19 +0900

    Feat: 기록페이지 프로필 코드 변경
    Former-commit-id: dcdf9689b49dca3e08c51e575deee72867588ddc

--- FE/src/pages/Memorize/Memorize.tsx
+++ FE/src/pages/Memorize/Memorize.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7d877db3bd1a856d6381e490693d82590c740b15222119c9ce99650a84aab43
-size 6245
+oid sha256:73b2480a4ac66d078c9acb9f17ddd05a08faca213349f2c874c876ae583f7bf1
+size 6271
