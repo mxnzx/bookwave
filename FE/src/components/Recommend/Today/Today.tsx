74163532baa88ef4590741aa85609644182af9e5
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-03 17:00:30 +0900

    Style: today recommend style fix
    Former-commit-id: f1ef4ad826ab6037958693baa7165e4d74d6d8c4

--- FE/src/components/Recommend/Today/Today.tsx
+++ FE/src/components/Recommend/Today/Today.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63e787b043f043d734e8b6db4e10d64bab4dd970c22be945f345cc815c029380
-size 2174
+oid sha256:15815a7c76f6bafa15761c35ce6137e4fdfcda9e309592ff9733d5b69e3fcca1
+size 2177
