ffb432be358e2c3ab2afd9db71e6381d9c3b41db
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-03 20:28:37 +0900

    Feat: 게시글 작성 빈칸알람, 선택모달 리코일 처리
    Former-commit-id: a8a003d6e1c015d6619f5cb9893bcabe704c7def

--- FE/src/pages/CreateRecordPage/CreateRecordPage.tsx
+++ FE/src/pages/CreateRecordPage/CreateRecordPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dd9ec852d56ae231ee38a5ce807bbec011668fe6a0e88fa923ddb46735d93b1
-size 5960
+oid sha256:495b23fc74f48196ee59f53bb0b164b0fe79e58b32c2a14ac8f0cd5938f41587
+size 6379
