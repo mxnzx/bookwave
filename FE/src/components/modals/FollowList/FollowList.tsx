0eb37046a4121268d944ec9174217c667fa6d70a
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-10-03 17:41:05 +0900

    Feat: 팔로우 모달 디자인 완료
    Former-commit-id: e05a04089ebb54a8269405781572d854779f7fe2

--- FE/src/components/modals/FollowList/FollowList.tsx
+++ FE/src/components/modals/FollowList/FollowList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d71240dcffa5b4d414ebb9ec6832daf999340a38a034f97eb2ac10be30b6ff3
+oid sha256:d0f6deb68ed0b1d3cfb5d15f8fab260e62b0bef69c966027842741ef12b19618
 size 2749
