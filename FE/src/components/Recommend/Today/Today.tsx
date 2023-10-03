6a25f85ecddfb349f66936eec8f68ba00fe82f60
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-03 17:29:55 +0900

    Feat: 오늘의 책 추천 구현 완료
    Former-commit-id: ca5b74fe300a1d6e4bd756c6bd6e5fb89c372764

--- FE/src/components/Recommend/Today/Today.tsx
+++ FE/src/components/Recommend/Today/Today.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15815a7c76f6bafa15761c35ce6137e4fdfcda9e309592ff9733d5b69e3fcca1
-size 2177
+oid sha256:7ef10b5db99030fe5ac4814c5875444781d3dec8ad849499965e1ce322f19e85
+size 2507
