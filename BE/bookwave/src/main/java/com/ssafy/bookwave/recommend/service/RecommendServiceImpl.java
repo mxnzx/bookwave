56fa4f5691d6bab3d95a75ad88ad9c7a485c639b
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-03 16:57:16 +0900

    Fix: Today Recommend Response 수정 - 일기 안썼으면 null로 response - 일기 썼으면 오늘의 책, emotion 보냄
    Former-commit-id: bafced6e5916c6653c65174be60b3b789b2aca66

--- BE/bookwave/src/main/java/com/ssafy/bookwave/recommend/service/RecommendServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/recommend/service/RecommendServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af467ee48384a767773d67071a0c5a9f2d95deba52f0c4795d36caa7dd6b83c4
-size 12961
+oid sha256:37d92cf44d8e3588f5f5657874e181b2f130f60e6d29e23a651b7f1e15e4e3ce
+size 13280
