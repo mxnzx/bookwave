fb9ec049f020e4ff39d89223f1a55da7f05f76fa
choijehyuk <cjh970324@naver.com>
2023-10-03 18:42:38 +0900

    Fix: 팔로우 팔로워 변경
    Former-commit-id: 0f81bbdfd64668a4e9d96dad8e6b50788d115eea

--- BE/bookwave/src/main/java/com/ssafy/bookwave/member/controller/FollowController.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/member/controller/FollowController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bc0eb67119716c5927a125e053edd1a628743a72884debd44173432e6a17d4f
-size 3864
+oid sha256:cb250f9943e52e6b3f4784c6da23348ebb0d9fe1f4a8761e2fb62d71aa28d995
+size 3937
