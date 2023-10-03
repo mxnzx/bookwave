23019fed27d8acdffe2896ca5b0e0901d1fd9b6f
choijehyuk <cjh970324@naver.com>
2023-10-03 20:46:14 +0900

    Fix: record , fastapi 통신
    Former-commit-id: b4a3743adbb382a341a9542766f442316505d3a2

--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd03410760d115a28989e18d7323db01f9193793f6236f3dd6d180cfdbbfa007
-size 19819
+oid sha256:ab49e2323a30ffb530d454e4c7402be7ec9ceede8ff927fc1f0c40c09550421d
+size 19818
