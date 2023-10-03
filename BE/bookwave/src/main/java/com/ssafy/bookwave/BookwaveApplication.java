8b0c7843d645c9eef624b535ee333be294c061f7
choijehyuk <cjh970324@naver.com>
2023-10-03 20:40:27 +0900

    Fix: record , fastapi 통신
    Former-commit-id: 351793992e93d27021227a6ed91af5ceb3e852fc

--- BE/bookwave/src/main/java/com/ssafy/bookwave/BookwaveApplication.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/BookwaveApplication.java
@@ -1,17 +1,3 @@
-package com.ssafy.bookwave;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@SpringBootApplication
-@EnableJpaAuditing
-@EnableScheduling
-public class BookwaveApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookwaveApplication.class, args);
-	}
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c077a8ab754a692d7275d7a212e01d0ca4237b241af74f673b3b3499e65742d9
+size 567
