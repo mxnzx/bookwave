f197709d1b735491488e10b2ea637d48edff2d8e
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-10-05 01:12:17 +0900

    Feat: 헤더 디자인 수정 중
    Former-commit-id: 75f96698993fc0caf5115fc2c333b80239dc43cd

--- FE/src/components/Common/Header/Header.tsx
+++ FE/src/components/Common/Header/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1256848ca102008eac2071cc928d9813fc9f04d684cfa4e9a1f6c42f2051d9e2
-size 5135
+oid sha256:1d7e6d9f270eacad5a650fe0abfd4776bf13519b96f81707d342cf8dcf54e77b
+size 4992
