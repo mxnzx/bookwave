01bb99e9a00b08a7e59468dffecf10256e0909e6
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-10-04 16:19:34 +0900

    Feat: 독후감 디테일 페이지 수정
    Former-commit-id: b195f896895cc3b73e09cde6b8367bf7f3d32399

--- FE/src/components/modals/RecordDetailModal/CommentComponent.tsx
+++ FE/src/components/modals/RecordDetailModal/CommentComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c89167bbf9b9556e128d446e7324ff7051d028ffafbbdb4fa84946764e0f40c
-size 4267
+oid sha256:03032613b807da5e24ca7766c0420fc1292ad16280bd4753a8a79ccef266d8c1
+size 4263
