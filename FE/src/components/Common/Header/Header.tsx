74e95bc6faabfb3c48c0cb8efbf2a2f6c44e543c
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-05 01:21:36 +0900

    Fix: 로그인 안된 상태에서 로고 누르면 로그인하도록
    Former-commit-id: f9fd5fb31dac048f64d9bf671373ddf2d94db045

--- FE/src/components/Common/Header/Header.tsx
+++ FE/src/components/Common/Header/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1256848ca102008eac2071cc928d9813fc9f04d684cfa4e9a1f6c42f2051d9e2
-size 5135
+oid sha256:718f692930bcc5e422ea68625c376be16c2c1daaff9c92c937193ed621ee587c
+size 5257
