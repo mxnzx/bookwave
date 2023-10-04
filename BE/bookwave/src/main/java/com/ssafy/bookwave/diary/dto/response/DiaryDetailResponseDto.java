d5d6d219e461895d9477e9d9af67cc80b2c8fd61
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-10-05 01:34:02 +0900

    Fix: 다이어리 상세 불러오기 할때 오늘의 책 id 보내기
    Former-commit-id: 305819fde232ce4e798f8de3748ca53d036e639c

--- BE/bookwave/src/main/java/com/ssafy/bookwave/diary/dto/response/DiaryDetailResponseDto.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/diary/dto/response/DiaryDetailResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3cf0a4302033ee22f20fe89a22034b4c134f063efc0c7114a745c08193520b1
-size 758
+oid sha256:369aa211453f47d06946d6c9d6d6aa47ac54257d8cf62bd7783eb1ac528dfde3
+size 791
