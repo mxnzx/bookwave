260147275eea3acbc53a51a0d58d6ba3bb60ba69
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-04 17:56:21 +0900

    Fix: 작성버튼
    Former-commit-id: 6a07ed5f5c31faccdd3ccde42261dbef6abe9ac4

--- FE/src/components/modals/FeedBookSelectModal/FeedBookSelect.tsx
+++ FE/src/components/modals/FeedBookSelectModal/FeedBookSelect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d804a1f9c8b08b290c491c3e60b17a3e54a292a5995077d732919f53a07efbb
-size 6553
+oid sha256:a3f991230fd7fcb9fb2211531d8d873d42160a56fb828b684f0ba3bce65ddf5d
+size 6554
--- FE/src/pages/FeedPage/FeedPage.styles.ts
+++ FE/src/pages/FeedPage/FeedPage.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0d2d014e9ce8434860ff23c11743d90d675c5ee7bc587e0ccd3e7ca87583fe9
-size 3331
+oid sha256:c576548823f0cbdecdf143f4bae121e4639862a20b226fe8ba123b3644acec78
+size 4096
