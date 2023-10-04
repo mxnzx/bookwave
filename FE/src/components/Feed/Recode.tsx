b4e01551502ca41445904c386dd5add9f0e30e0b
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-04 16:09:35 +0900

    Feat: 반응형 footer
    Former-commit-id: 78bee04634697c1144e20d9296a55cc64b7475ac

--- FE/src/components/Feed/Recode.tsx
+++ FE/src/components/Feed/Recode.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db50dc0c6ef40d01e34588a6fd6c179a612f02e4155be0dd662e94e927c999af
-size 6627
+oid sha256:39e2451614ec6a6197f7eb3f9767694e5ffc040caa195e06c34a1d142efbd61b
+size 6726
