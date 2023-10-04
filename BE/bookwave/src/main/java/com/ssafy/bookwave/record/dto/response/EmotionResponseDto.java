a1d8754a3ac7048cb9b1c20421765aae46dc1a96
junho <sseq007@naver.com>
2023-10-04 12:42:46 +0900

    Feat: 독후감 작성 시 fast api update 수정
    Former-commit-id: 2283bc6b71f621740873f50d4ac424beb27599db

---
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/dto/response/EmotionResponseDto.java
@@ -0,0 +1,10 @@
+package com.ssafy.bookwave.record.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class EmotionResponseDto {
+
+    private int id;
+    private String emotion;
+}
--- BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
+++ BE/bookwave/src/main/java/com/ssafy/bookwave/record/service/RecordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad74a48545d352eac33839a65931ce139461fd86bc310ce62da4ef3af22b3774
-size 19833
+oid sha256:870d027cb70a70eb80f5782189bf3dee0737466af090817c90d736ad5bf5240c
+size 20127
