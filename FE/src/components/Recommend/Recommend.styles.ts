db00e958682e93a4214a02a113f321cc29defeb2
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-04 15:56:29 +0900

    Feat:선호 장르 변경 기능 추가
    Former-commit-id: 8a6478f42687ac54642192199ce780f3c74135e5

--- FE/src/components/Recommend/Recommend.styles.ts
+++ FE/src/components/Recommend/Recommend.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:687850c5904e189d47af3b5e560b8014655807feb548611a8ff03d8899421c6e
-size 7724
+oid sha256:5d9a4058714f58fcff1ec2d40bb5be3c40dccd40e7f4a28c3a04749cf31ad13f
+size 8369
