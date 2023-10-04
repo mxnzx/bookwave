59a5da90fec959897c6acd071904286bdcd1f23f
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-04 13:42:36 +0900

    Feat: CSS Refactoring_4
    Former-commit-id: 96e8bffc21eed70541a51933ad733edfbb5f8707

--- FE/src/components/Feed/BBTI.styles.ts
+++ FE/src/components/Feed/BBTI.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:accf99f549be4fa72ae4f47fda187d4e6dcfe639cf73c6b2232c39ff11b9a5c8
-size 2958
+oid sha256:3180abbe2b026821505f6cc844216cc4c6f1713c94f8bf0d2b063d258641da71
+size 2874
--- FE/src/components/Feed/BBTI.tsx
+++ FE/src/components/Feed/BBTI.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b6a37d8dab986b4717d9fbbed6d8074bec89c445214fd390071e07a2c693879
+oid sha256:b704ed11ae86073a79bdd75a22a3126ab3826f48f2ec9b8b34853bca6850c253
 size 4841
