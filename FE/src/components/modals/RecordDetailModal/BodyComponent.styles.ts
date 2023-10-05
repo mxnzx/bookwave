fe15691195fd55e43f82a4fdba8a43c52cda3a3e
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-05 17:29:22 +0900

    Fix: style 수정
    Former-commit-id: 0ea73a7fcc4e5447a1a1829244d2646c2b4b9f4e

--- FE/src/components/modals/RecordDetailModal/BodyComponent.styles.ts
+++ FE/src/components/modals/RecordDetailModal/BodyComponent.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:984e0cc6ccce75c1b1a7deb664f67e6488aa77b17131a09d92d594091151f706
-size 2866
+oid sha256:946c20f85ed0c334a82856ee0489756a6cf6c451be6cc1db580830316259322c
+size 2881
