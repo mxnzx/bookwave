3b41699124a39645daa848bda7bb7ce631902a3e
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-10-06 03:28:24 +0900

    Feat: 기록하기 수정
    Former-commit-id: 6fa8247e9a473dcfc4709870dcaa8a265e80ee4c

--- FE/src/components/Profile/ProfileReminder/ProfileDiary.styles.ts
+++ FE/src/components/Profile/ProfileReminder/ProfileDiary.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba80c2ede9140a5a6346c9a80e0a658ac1a17e8419ff9bc7750419cd7c0ce9a7
-size 1065
+oid sha256:62ed99f07d8092d0e87d85ad3953cddc6c4d114d3ba8a30dc7eb975e5f1b8c4a
+size 1289
