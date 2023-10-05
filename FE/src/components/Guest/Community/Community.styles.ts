c53459a1b977947c2b50643f4e6cca3b141a58e9
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-10-06 01:18:39 +0900

    Feat: 게스트 페이지 수정
    Former-commit-id: 564a62d3cbb7b9a951e83eea468f6afe4e3bf6b3

--- FE/src/components/Guest/Community/Community.styles.ts
+++ FE/src/components/Guest/Community/Community.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9b649d55e1287fb7f25b92be62cbac50bc5331bb0da745c2b29a939d55eefb7
-size 1973
+oid sha256:1c3217b51e97068b5972b70ae3731fd9ccc8686872b737874f2109be119789af
+size 1845
