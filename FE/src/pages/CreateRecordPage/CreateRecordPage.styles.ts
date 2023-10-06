ff73f859bfb67d0a2af8f869c1d002aa168f0847
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-06 11:01:34 +0900

    Fix: Style 수정
    Former-commit-id: cfd8708c2dab66b0dbd77332c7ea0734364fa39b

--- FE/src/pages/CreateRecordPage/CreateRecordPage.styles.ts
+++ FE/src/pages/CreateRecordPage/CreateRecordPage.styles.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc65a9a920f9ff316be119fabd43ea7458c45bb1f541b2451362109e9fc15595
-size 4485
+oid sha256:470b10dcdd512dc74bd51d5b0fd10a83c2ccf04558b470b0f278bbb1a213036b
+size 4524
